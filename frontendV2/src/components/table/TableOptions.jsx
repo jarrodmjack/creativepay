eed4ea7c368674089ba46fd3af56786f115d240e
jarrodmjack <jarrodmjack@gmail.com>
2023-09-28 09:57:37 -0400

    add employee filter for timepunches

--- frontendV2/src/components/table/TableOptions.jsx
+++ frontendV2/src/components/table/TableOptions.jsx
@@ -1,66 +1,124 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from "react"
+import { useAuthContext } from "../../hooks/useAuthContext"
 
-const TableOptions = ({ handlePrint, handleFilterTimePunches, handleResetFilter }) => {
+const TableOptions = ({
+	handlePrint,
+	handleFilterTimePunches,
+	handleResetFilter,
+}) => {
+	const { user } = useAuthContext()
+	const [fromDate, setFromDate] = useState("")
+	const [toDate, setToDate] = useState("")
+	const [employeeList, setEmployeeList] = useState([])
+	const [employeeFilter, setEmployeeFilter] = useState("")
 
-  const [fromDate, setFromDate] = useState('')
-  const [toDate, setToDate] = useState('')
+	useEffect(() => {
+		const fetchEmployeeList = async () => {
+			const response = await fetch("/api/home/", {
+				method: "GET",
+				headers: {
+					Authorization: `Bearer ${user.token}`,
+				},
+			})
+			const data = await response.json()
+			setEmployeeList(data)
+		}
+		fetchEmployeeList()
+	}, [])
 
-  return (
-    <div className='flex flex-col gap-4'>
-      <span className='text-2xl font-semibold'>Filter by date</span>
-      <form onSubmit={(e) => {
-        e.preventDefault()
-        handleFilterTimePunches(fromDate, toDate)
-      }}>
-        <div className='flex flex-col gap-4'>
-          <div className='flex gap-10'>
-            <div className='flex flex-col'>
-              <label>From</label>
-              <input
-                required
-                type="date"
-                onChange={(e) => setFromDate(e.target.value)}
-                value={fromDate}
-              />
-            </div>
-            <div className='flex flex-col'>
-              <label>To</label>
-              <input
-                required
-                type="date"
-                onChange={(e) => setToDate(e.target.value)}
-                value={toDate}
-              />
-            </div>
-          </div>
-          <div className='flex gap-4'>
-            <button
-              type='submit'
-              className="btn btn-success w-40"
-            >Apply filter
-            </button>
-            <button
-              onClick={(e) => {
-                e.preventDefault()
-                handleResetFilter()
-                setFromDate('')
-                setToDate('')
-              }}
-              className='btn btn-accent w-40'>
-              Reset Filter
-            </button>
-          </div>
-        </div>
-      </form>
-      <div className='flex justify-end'>
-        <button
-          onClick={handlePrint}
-          className='btn btn-secondary w-40'>
-          Print table
-        </button>
-      </div>
-    </div>
-  )
+	return (
+		<div className="flex flex-col gap-4">
+			<div>
+				<span className="text-2xl font-semibold">
+					Filter by date and employee
+				</span>
+				<form
+					className="border p-4"
+					onSubmit={(e) => {
+						e.preventDefault()
+						handleFilterTimePunches(
+							fromDate,
+							toDate,
+							employeeFilter
+						)
+					}}
+				>
+					<div className="flex flex-col gap-4">
+						<div className="flex gap-10">
+							<div className="flex flex-col">
+								<label>From</label>
+								<input
+									required
+									type="date"
+									onChange={(e) =>
+										setFromDate(e.target.value)
+									}
+									value={fromDate}
+								/>
+							</div>
+							<div className="flex flex-col">
+								<label>To</label>
+								<input
+									required
+									type="date"
+									onChange={(e) => setToDate(e.target.value)}
+									value={toDate}
+								/>
+							</div>
+							<div className="flex flex-col">
+								<label htmlFor="">Employee</label>
+								<select
+									required
+									className=""
+									onChange={(e) =>
+										setEmployeeFilter(e.target.value)
+									}
+								>
+									<option disabled selected>
+										Employee Name
+									</option>
+									{employeeList &&
+										employeeList.map((employee, i) => (
+											<option key={i}>
+												{employee.name}
+											</option>
+										))}
+								</select>
+							</div>
+						</div>
+						<div className="flex gap-4">
+							<button
+								type="submit"
+								className="btn btn-success w-40"
+							>
+								Apply filter
+							</button>
+							<button
+								onClick={(e) => {
+									e.preventDefault()
+									handleResetFilter()
+									setEmployeeFilter("")
+									setFromDate("")
+									setToDate("")
+								}}
+								className="btn btn-accent w-40"
+							>
+								Reset Filter
+							</button>
+						</div>
+					</div>
+				</form>
+			</div>
+			<div className="flex justify-end">
+				<button
+					onClick={handlePrint}
+					className="btn btn-secondary w-40"
+				>
+					Print table
+				</button>
+			</div>
+		</div>
+	)
 }
 
 export default TableOptions
--- frontendV2/src/components/table/TimePunchTable.jsx
+++ frontendV2/src/components/table/TimePunchTable.jsx
@@ -5,141 +5,173 @@ import TimePunchTableHeader from "./TimePunchTableHeader"
 import TableOptions from "./TableOptions"
 import TimePunchCreationForm from "../forms/TimePunchCreationForm"
 import { useAuthContext } from "../../hooks/useAuthContext"
-import { useReactToPrint } from 'react-to-print'
+import { useReactToPrint } from "react-to-print"
 
 const TimePunchTable = ({ timePunches }) => {
+	const { user } = useAuthContext()
+	const [tableTimePunches, setTableTimePunches] = useState(timePunches)
+	const [filteredTimePunches, setFilteredTimePunches] = useState([])
+	const [currentPage, setCurrentPage] = useState(1)
+	const [timepunchesPerPage, settimepunchesPerPage] = useState(12)
+	// Pagination
+	const indexOfLastTimePunch = currentPage * timepunchesPerPage
+	const indexOfFirstTimepunch = indexOfLastTimePunch - timepunchesPerPage
+	const currentTimepunches = tableTimePunches.slice(
+		indexOfFirstTimepunch,
+		indexOfLastTimePunch
+	)
 
-    const { user } = useAuthContext()
-    const [tableTimePunches, setTableTimePunches] = useState(timePunches)
-    const [filteredTimePunches, setFilteredTimePunches] = useState([])
-    const [currentPage, setCurrentPage] = useState(1)
-    const [timepunchesPerPage, settimepunchesPerPage] = useState(12)
+	const paginate = (pageNumber) => {
+		setCurrentPage(pageNumber)
+	}
 
-    // Pagination
-    const indexOfLastTimePunch = currentPage * timepunchesPerPage
-    const indexOfFirstTimepunch = indexOfLastTimePunch - timepunchesPerPage
-    const currentTimepunches = tableTimePunches.slice(indexOfFirstTimepunch, indexOfLastTimePunch)
+	const handleSubmitTimePunch = async (timePunch) => {
+		try {
+			const response = await fetch("/api/punch", {
+				method: "POST",
+				body: JSON.stringify(timePunch),
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${user.token}`,
+				},
+			})
+			if (response.ok) {
+				const newTimePunch = await response.json()
+				setTableTimePunches([newTimePunch, ...currentTimepunches])
+			}
+		} catch (err) {
+			window.alert(
+				"There was an issue submitting the timepunch. Please ensure the information entered is correct and try again"
+			)
+		}
+	}
 
-    const paginate = (pageNumber) => {
-        setCurrentPage(pageNumber)
-    }
+	const handleTogglePaid = async (id, index) => {
+		try {
+			const response = await fetch(`/api/punch`, {
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${user.token}`,
+				},
+				body: JSON.stringify({ id }),
+			})
+			const data = await response.json()
+			if (response.ok) {
+				const updatedTimePunches = [...currentTimepunches]
+				updatedTimePunches[index].paid = data.newPaidStatus
+				setTableTimePunches(updatedTimePunches)
+			}
+		} catch (error) {
+			console.error(error)
+		}
+	}
 
-    const handleSubmitTimePunch = async (timePunch) => {
-        try {
-            const response = await fetch('/api/punch', {
-                method: 'POST',
-                body: JSON.stringify(timePunch),
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${user.token}`
-                }
-            })
-            if (response.ok) {
-                const newTimePunch = await response.json()
-                setTableTimePunches([newTimePunch, ...currentTimepunches])
-            }
-        } catch (err) {
-            window.alert('There was an issue submitting the timepunch. Please ensure the information entered is correct and try again')
-        }
-    }
+	const handleFilterTimePunches = (fromDate, toDate, employeeName) => {
+		fromDate = new Date(fromDate)
+		toDate = new Date(toDate)
+		if (!employeeName) {
+			setFilteredTimePunches(
+				tableTimePunches.filter((timepunch) => {
+					let timepunchDate = new Date(timepunch.date)
+                    console.log('tp: ', timepunch)
+					if (timepunchDate <= toDate && timepunchDate >= fromDate) {
+						return timepunch
+					}
+				})
+			)
+		} else {
+			setFilteredTimePunches(
+				tableTimePunches.filter((timepunch) => {
+					let timepunchDate = new Date(timepunch.date)
+					if (timepunchDate <= toDate && timepunchDate >= fromDate && timepunch.employeeName === employeeName) {
+						return timepunch
+					}
+				})
+			)
+		}
+	}
 
-    const handleTogglePaid = async (id, index) => {
-        try {
-            const response = await fetch(`/api/punch`, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${user.token}`
-                },
-                body: JSON.stringify({ id })
-            });
-            const data = await response.json()
-            if (response.ok) {
-                const updatedTimePunches = [...currentTimepunches]
-                updatedTimePunches[index].paid = data.newPaidStatus
-                setTableTimePunches(updatedTimePunches)
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    }
+	const resetTimePunchFilter = () => {
+		setFilteredTimePunches([])
+	}
 
-    const handleFilterTimePunches = (fromDate, toDate) => {
-        fromDate = new Date(fromDate)
-        toDate = new Date(toDate)
-        setFilteredTimePunches(tableTimePunches.filter((timepunch) => {
-            let timepunchDate = new Date(timepunch.date)
-            if (timepunchDate <= toDate && timepunchDate >= fromDate) {
-                return timepunch
-            }
-        }))
-    }
+	const handleDeleteTimePunch = async (id) => {
+		try {
+			const response = await fetch(`/api/punch`, {
+				method: "DELETE",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${user.token}`,
+				},
+				body: JSON.stringify({ id }),
+			})
+			if (response.ok) {
+				setTableTimePunches(
+					currentTimepunches.filter(
+						(timePunch) => timePunch._id !== id
+					)
+				)
+			} else {
+				window.alert("There was an issue deleting. Please try again")
+			}
+		} catch (error) {
+			console.error(error)
+		}
+	}
 
-    const resetTimePunchFilter = () => {
-        setFilteredTimePunches([])
-    }
+	const componentRef = useRef(null)
+	const handlePrint = useReactToPrint({
+		content: () => componentRef.current,
+		documentTitle: "timepunch-data",
+	})
 
-    const handleDeleteTimePunch = async (id) => {
-        try {
-            const response = await fetch(`/api/punch`, {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${user.token}`
-                },
-                body: JSON.stringify({ id })
-            });
-            if (response.ok) {
-                setTableTimePunches(currentTimepunches.filter(timePunch => timePunch._id !== id))
-            } else {
-                window.alert('There was an issue deleting. Please try again')
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-    const componentRef = useRef(null)
-    const handlePrint = useReactToPrint({
-        content: () => componentRef.current,
-        documentTitle: 'timepunch-data',
-    })
-
-    return (
-        <div>
-            <div className="overflow-x-auto shadow-xl">
-                <TimePunchCreationForm handleSubmitTimePunch={handleSubmitTimePunch} />
-                <TableOptions handlePrint={handlePrint} handleFilterTimePunches={handleFilterTimePunches} handleResetFilter={resetTimePunchFilter} />
-                <table ref={componentRef} className="table w-full">
-                    <TimePunchTableHeader />
-                    <tbody>
-                        {
-                            filteredTimePunches.length > 0 ? (
-                                filteredTimePunches.map((timePunch, i) => (
-                                    <TimePunchTableRow
-                                        key={i}
-                                        index={i}
-                                        togglePaid={handleTogglePaid}
-                                        timePunch={timePunch}
-                                        handleDelete={handleDeleteTimePunch} />
-                                ))
-                            ) : (
-                                currentTimepunches.map((timePunch, i) => (
-                                    <TimePunchTableRow
-                                        key={i}
-                                        index={i}
-                                        togglePaid={handleTogglePaid}
-                                        timePunch={timePunch}
-                                        handleDelete={handleDeleteTimePunch} />
-                                ))
-                            )
-                        }
-                    </tbody>
-                </table>
-            </div>
-            {!filteredTimePunches.length && <Pagination timepunchesPerPage={timepunchesPerPage} totalTimePunches={timePunches.length} paginate={paginate} currentPage={currentPage} />}
-        </div>
-    )
+	return (
+		<div>
+			<div className="overflow-x-auto shadow-xl">
+				<TimePunchCreationForm
+					handleSubmitTimePunch={handleSubmitTimePunch}
+				/>
+				<TableOptions
+					handlePrint={handlePrint}
+					handleFilterTimePunches={handleFilterTimePunches}
+					handleResetFilter={resetTimePunchFilter}
+				/>
+				<table ref={componentRef} className="table w-full">
+					<TimePunchTableHeader />
+					<tbody>
+						{filteredTimePunches.length > 0
+							? filteredTimePunches.map((timePunch, i) => (
+									<TimePunchTableRow
+										key={i}
+										index={i}
+										togglePaid={handleTogglePaid}
+										timePunch={timePunch}
+										handleDelete={handleDeleteTimePunch}
+									/>
+							  ))
+							: currentTimepunches.map((timePunch, i) => (
+									<TimePunchTableRow
+										key={i}
+										index={i}
+										togglePaid={handleTogglePaid}
+										timePunch={timePunch}
+										handleDelete={handleDeleteTimePunch}
+									/>
+							  ))}
+					</tbody>
+				</table>
+			</div>
+			{!filteredTimePunches.length && (
+				<Pagination
+					timepunchesPerPage={timepunchesPerPage}
+					totalTimePunches={timePunches.length}
+					paginate={paginate}
+					currentPage={currentPage}
+				/>
+			)}
+		</div>
+	)
 }
 
 export default TimePunchTable
